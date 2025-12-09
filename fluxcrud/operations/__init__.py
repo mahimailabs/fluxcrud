78c1d683b0b0455fbe8ffb1ea01859b10ac17ecc
Mahimai Raja J <mahimairaja3@gmail.com>
2025-12-09 10:36:47 -0500

    feat: defined crud operations

---
+++ fluxcrud/operations/__init__.py
@@ -0,0 +1,6 @@
+from .create import CreateMixin
+from .delete import DeleteMixin
+from .read import ReadMixin
+from .update import UpdateMixin
+
+__all__ = ["CreateMixin", "DeleteMixin", "ReadMixin", "UpdateMixin"]
